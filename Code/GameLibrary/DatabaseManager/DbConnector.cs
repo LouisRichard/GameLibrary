8bf90f63d63ce10692801ec96c5e9d63f2e04a77
Louis Richard <Louis.Richard@tutanota.com>
2019-11-28 12:14:19 +0100

    (!!!)[BUGFIX] Major modifications in the Dbconnector
    Instead of creating the DB file in ConnectToDatabase, calls another method to do so.
    then creates the tables needed for the db to works.
    
    TODO : CreateTable method

--- Code/GameLibrary/DatabaseManager/DbConnector.cs
+++ Code/GameLibrary/DatabaseManager/DbConnector.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO;
 using System.Data.SQLite;
+using System;
 
 namespace GameLibrary
 {
@@ -13,11 +14,11 @@ namespace GameLibrary
         /// <returns>Null if database is getting closed</returns>
         internal static SQLiteCommand ConnectToDatabase(bool close)
         {
+            bool createTable = false;
             if (!File.Exists("GLdb.db3"))
             {
-                System.Data.SQLite.SQLiteConnection.CreateFile(
-                    Path.Combine(
-                        Directory.GetCurrentDirectory(), "GLdb.db3")); //Create the database
+                CreateDatabase();
+                createTable = true;
             }
 
             System.Data.SQLite.SQLiteConnection conn = new System.Data.SQLite.SQLiteConnection("data source=GLdb.db3");
@@ -26,6 +27,10 @@ namespace GameLibrary
             if (!close)
             {
                 conn.Open(); //Open connection to the SQLite database
+                if (createTable)
+                {
+                    CreateTable(cmd);
+                }
                 return cmd;
             }
             else
@@ -34,6 +39,25 @@ namespace GameLibrary
                 conn.Close();
                 return cmd;
             }
+        }
+
+        /// <summary>
+        /// Create the DB File
+        /// </summary>
+        private static void CreateDatabase()
+        {
+            System.Data.SQLite.SQLiteConnection.CreateFile(
+                    Path.Combine(
+                        Directory.GetCurrentDirectory(), "GLdb.db3")); //Create the database
+        }
+        /// <summary>
+        /// Create the tables needed fot the database to works properly
+        /// </summary>
+        /// <param name="cmd">SQLiteCommand cmd</param>
+        private static void CreateTable(SQLiteCommand cmd)
+        {
+            //TODO CREATE TABLES NEEDED FOR THE DB
+            throw new NotImplementedException();
         }
     }
 }
