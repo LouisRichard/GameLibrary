b47c3e6aa5927226a793c70a4d4888c2d48640f7
Louis Richard <Louis.Richard@tutanota.com>
2019-11-28 14:56:22 +0100

    [OTHER] Changed returned value in SELECT
    Select now returns a List<string> instead of a SQLiteDataReader

--- Code/GameLibrary/DatabaseManager/ExecuteQuery.cs
+++ Code/GameLibrary/DatabaseManager/ExecuteQuery.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Data.SQLite;
+using System.Data;
 
 namespace GameLibrary
 {
@@ -11,15 +12,22 @@ namespace GameLibrary
         /// Execute a Select query
         /// </summary>
         /// <param name="query">Select query to execute</param>
-        /// <returns>Query result as an SQLiteDataReader</returns>
-        public static SQLiteDataReader Select(string query)
+        /// <returns>Query result as an String List</returns>
+        public static List<string> Select(string query)
         {
             SQLiteCommand command = DbConnector.ConnectToDatabase(false); //open db connection
-
+            List<string> result = new List<string>();
+            DataTable dt = new DataTable();
             command.CommandText = query;
-            System.Data.SQLite.SQLiteDataReader reader = command.ExecuteReader();
-            _ = DbConnector.ConnectToDatabase(true); //close db connection  Using Ignore(_) as we don't care what it returns.
-            return reader;
+            using (SQLiteDataReader reader = command.ExecuteReader())
+            {
+                while(reader.Read())
+                {
+                    result.Add(Convert.ToString(reader));
+                }
+            }
+                _ = DbConnector.ConnectToDatabase(true); //close db connection  Using Ignore(_) as we don't care what it returns.
+            return result;
         }
     }
 }
