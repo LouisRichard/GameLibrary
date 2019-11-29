d5ff2fbf4e318d54bab7e038679e511f2a5da44c
Louis Richard <Louis.Richard@tutanota.com>
2019-11-29 09:34:28 +0100

    [FEATURE] method LoginRequest

--- Code/GameLibrary/DataManager/UserManager.cs
+++ Code/GameLibrary/DataManager/UserManager.cs
@@ -1,8 +1,18 @@
 ﻿using System;
 
-namespace DataManager
+namespace GameLibrary
 {
-    public class Class1
+    public class UserManager
     {
+        public static bool LoginRequest(string email, string password)
+        {
+            string testLoginQuery = @"SELECT * FROM [users] WHERE [email] = " + email + " AND PASSWORD = " + password;
+
+            if(ExecuteQuery.Select(testLoginQuery) != null)
+            {
+                return true;
+            }
+            throw new Exception("User doesn't exist");
+        }
     }
 }
