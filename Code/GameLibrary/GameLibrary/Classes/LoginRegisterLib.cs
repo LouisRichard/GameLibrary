e4402b8b80ab088f0145427b69e76480934630ed
JonasHau <35954302+JonasHau@users.noreply.github.com>
2019-11-29 10:27:57 +0100

    Created LoginRegisterLib

---
+++ Code/GameLibrary/GameLibrary/Classes/LoginRegisterLib.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace GameLibrary
+{
+    /// <summary>
+    /// This class will have different methods used in the login/register process
+    /// </summary>
+    public class LoginRegisterLib
+    {
+        /// <summary>
+        /// Check if a mail is valid
+        /// </summary>
+        /// <param name="email">the mail that must be checked</param>
+        /// <returns>boolean value: true if the mail is valid, false if it isnt</returns>
+        public bool ValidMail(string email)
+        {
+            return Regex.IsMatch(email, @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", RegexOptions.IgnoreCase);
+        }
+    }
+}
