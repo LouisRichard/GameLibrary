12ddff84db6a05a7001ce96569994b492ae4cf09
Louis Richard <Louis.Richard@tutanota.com>
2019-11-29 14:05:35 +0100

    [TESTS] email valid and not valid

--- Code/GameLibrary/Tests/TestLoginRegister/TestLogin.cs
+++ Code/GameLibrary/Tests/TestLoginRegister/TestLogin.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GameLibrary;
 
 namespace TestLoginRegister
 {
@@ -9,13 +10,21 @@ namespace TestLoginRegister
         [TestMethod]
         public void TestEmailNotValid()
         {
+            LoginRegisterLib lib = new LoginRegisterLib();
+            string notValidEmail = "Not So Valid Email, Is It?";
+            bool valid = lib.ValidMail(notValidEmail);
 
+            Assert.AreEqual(valid, false);
         }
 
         [TestMethod]
         public void TestEmailValid()
         {
+            LoginRegisterLib lib = new LoginRegisterLib();
+            string validEmail = "veryvalid@email.com";
+            bool valid = lib.ValidMail(validEmail);
 
+            Assert.AreEqual(valid, true);
         }
     }
 }
