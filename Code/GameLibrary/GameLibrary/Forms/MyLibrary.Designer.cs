183a19124c6b92aa6664ce02d05ae08160468303
JonasHau <35954302+JonasHau@users.noreply.github.com>
2019-12-03 10:46:52 +0100

    Added icons to the forms

--- Code/GameLibrary/GameLibrary/Forms/MyLibrary.Designer.cs
+++ Code/GameLibrary/GameLibrary/Forms/MyLibrary.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MyLibrary));
             this.SuspendLayout();
             // 
             // MyLibrary
@@ -35,6 +36,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MyLibrary";
             this.Text = "My library";
             this.ResumeLayout(false);
--- Code/GameLibrary/GameLibrary/Forms/RegisterValid.Designer.cs
+++ Code/GameLibrary/GameLibrary/Forms/RegisterValid.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RegisterValid));
             this.lblTitle = new System.Windows.Forms.Label();
             this.lblConfirm = new System.Windows.Forms.Label();
             this.btnSignIn = new System.Windows.Forms.Button();
@@ -73,6 +74,7 @@
             this.Controls.Add(this.lblConfirm);
             this.Controls.Add(this.lblTitle);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(800, 300);
             this.MinimumSize = new System.Drawing.Size(800, 300);
