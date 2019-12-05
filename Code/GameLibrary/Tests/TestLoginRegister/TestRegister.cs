33266c1a0d1325b203ef2ff2ff7839d1669277dd
LouisRichard <louis.richard@tutanota.com>
2019-12-05 21:06:49 +0100

    [NEW] new testsmethods

--- Code/GameLibrary/Tests/TestLoginRegister/TestRegister.cs
+++ Code/GameLibrary/Tests/TestLoginRegister/TestRegister.cs
@@ -12,6 +12,7 @@ namespace TestLoginRegister
         [TestMethod]
         public void TestRegisterUserThatDoesntExist()
         {
+            //If this test fails, try deleting your GLdb.db3 file in Code\GameLibrary\Tests\TestLoginRegister\bin\Debug
             string testUserEmail = @"testregister1@domain.ch";
             string testUserPassword = "password";
             string testUserPasswordConfirm = "password";
@@ -46,6 +47,48 @@ namespace TestLoginRegister
             bool success = false;
 
             success = UserManager.RegisterRequest(testUserEmail, testUserPassword, testUserPasswordConfirm);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(EmptyFieldException))]
+        public void TestRegiserWithoutEmail()
+        {
+            string testUserEmail = "";
+            string password = "root";
+
+            bool success = UserManager.RegisterRequest(testUserEmail, password, password);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(EmptyFieldException))]
+        public void RegisterUserWithoutPasswords()
+        {
+            string testUserEmail = "testUser@domain.ch";
+            string password = "";
+
+            bool success = UserManager.RegisterRequest(testUserEmail, password, password);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(EmptyFieldException))]
+        public void RegisterUserWithoutConfirmPassword()
+        {
+            string testUserEmail = "testUser@domain.ch";
+            string password = "Password";
+            string confirm = "";
+
+            bool success = UserManager.RegisterRequest(testUserEmail, password, confirm);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(EmptyFieldException))]
+        public void RegisterUserWithoutPasswordWithConfirm()
+        {
+            string testUserEmail = "testUser@domain.ch";
+            string password = "";
+            string confirm = "confirmPassword";
+
+            bool success = UserManager.RegisterRequest(testUserEmail, password, confirm);
         }
     #endregion
     }
