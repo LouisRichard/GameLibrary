fc1426a4d6c0f238bd456ce12882d0fd4d3a253f
LouisRichard <louis.richard@tutanota.com>
2019-12-19 14:53:06 +0100

    [NEW] Type Game

---
+++ Code/GameLibrary/DataManager/Classes/Game.cs
@@ -0,0 +1,36 @@
+﻿namespace DataManager
+{
+    public class Game
+    {
+        #region attributes
+
+        /// <summary>
+        /// The user's email.
+        /// </summary>
+        internal string title;
+        /// <summary>
+        /// 
+        /// </summary>
+        internal string platform;
+        /// <summary>
+        /// 
+        /// </summary>
+
+        #endregion attributes
+
+        #region constructors
+
+        /// <summary>
+        /// User with every fields filled constructor.
+        /// </summary>
+        /// <param name="username">the user's username.</param>
+        /// <param name="password">the user's password.</param>
+        /// <param name="rePassword">the user's password confirmed.</param>
+        public Game(string title, string platorm)
+        {
+            this.title = title;
+            this.platform = platorm;
+        }
+    }
+}
+        #endregion
